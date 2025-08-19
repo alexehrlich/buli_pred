0bbb8426321d4382c66f3b9f636f53afb8102207
Alexander Ehrlich <50752674+alexehrlich@users.noreply.github.com>
2025-08-19 17:15:57 +0200

    pretty print

--- src/data/Scraper.py
+++ src/data/Scraper.py
@@ -7,7 +7,6 @@ import requests
 import os
 import json
 from io import StringIO
-from playwright.sync_api import sync_playwright
 
 class Scraper:
     def __init__(self):
@@ -30,88 +29,6 @@ class Scraper:
                     f.close()
                 else:
                     print(f"could not find {team}")
-
-
-    # def scrape_match_data(self, years, from_date=None, to_date=pd.to_datetime('today').normalize(), save_to_filepath=None):
-    #     all_matches = []
-    #     latest_year = years[0]
-    #     standings_url = f"https://fbref.com/en/comps/20/{latest_year - 1}-{latest_year}/{latest_year - 1}-{latest_year}-Bundesliga-Stats"
-
-    #     with sync_playwright() as p:
-    #         browser = p.chromium.launch(headless=True)
-    #         context = browser.new_context(user_agent="Mozilla/5.0 ... Chrome/123.0 ...")
-    #         page = context.new_page()
-
-    #         for year in years:
-    #             print(f"Year: {year}")
-    #             page.goto(standings_url, wait_until="domcontentloaded")
-    #             html = page.content()
-    #             soup = BeautifulSoup(html, 'html.parser')
-
-    #             standings_table = soup.select('table.stats_table')[0]
-    #             links = standings_table.find_all('a')
-    #             links = [l.get('href') for l in links]
-    #             links = [l for l in links if '/squads' in l]
-    #             team_urls = [f"http://fbref.com{l}" for l in links]
-
-    #             previous_season = soup.select('a.prev')[0].get('href')
-    #             standings_url = f"https://fbref.com{previous_season}"
-
-    #             for team_url in team_urls:
-    #                 team_name = team_url.split('/')[-1].replace('-Stats', '').replace('-', ' ')
-    #                 print(f"Team: {team_name}")
-                    
-    #                 page.goto(team_url, wait_until="domcontentloaded")
-    #                 html = page.content()
-    #                 matches = pd.read_html(StringIO(html), match='Scores & Fixtures')[0]
-    #                 matches = matches.drop(matches[matches['Date'] == 'Date'].index)
-
-    #                 soup = BeautifulSoup(html, 'html.parser')
-    #                 with open('./out.html', 'w') as f:
-    #                     f.write(html)
-    #                 links = soup.find_all('a')
-    #                 links = [l.get('href') for l in links if l]
-    #                 links = [l for l in links if '/all_comps/shooting' in l]
-
-    #                 if len(links) > 0:
-    #                     page.goto(f"http://fbref.com{links[0]}", wait_until="domcontentloaded")
-    #                     html = page.content()
-    #                     shooting = pd.read_html(StringIO(html), match='Shooting')[0]
-    #                     shooting.columns = shooting.columns.droplevel()
-                        
-    #                     required_cols = ['Date', 'Sh', 'SoT', 'Dist', 'PK', 'FK', 'PKatt']
-    #                     for col in required_cols:
-    #                         if col not in shooting.columns:
-    #                             shooting[col] = pd.NA
-    #                     shooting_sub = shooting[required_cols]
-    #                     team_data = matches.merge(shooting_sub, on='Date', how='left')
-    #                 else:
-    #                     team_data = matches
-
-    #                 team_data["Date"] = pd.to_datetime(team_data["Date"], errors="coerce")
-    #                 team_data["Season"] = year
-    #                 team_data["Team"] = team_name
-    #                 if from_date is not None:
-    #                     team_data = team_data[team_data["Date"].between(from_date, to_date, inclusive='right')]
-    #                 team_data = team_data.drop(team_data[team_data["Date"] == "Date"].index)
-    #                 all_matches.append(team_data)
-    #                 time.sleep(15)  # kleine Pause zwischen Teams, Anti-Bot
-
-    #         browser.close()
-
-    #     match_df = pd.concat(all_matches)
-    #     if save_to_filepath:
-    #         with open('./data/raw/fbref_api_name_to_team_map.json') as f:
-    #             fbref_team_name_mapping = json.load(f)
-    #             match_df.columns = [c.lower() for c in match_df.columns]
-    #             match_df['team'] = match_df['team'].apply(lambda x: fbref_team_name_mapping[x])
-    #             if os.path.exists(save_to_filepath):
-    #                 existing_df = pd.read_csv(save_to_filepath)
-    #                 match_df = pd.concat([existing_df, match_df])
-    #             match_df['date'] = pd.to_datetime(match_df['date'])
-    #             match_df.sort_values('date').to_csv(save_to_filepath, index=False)
-
-    #     return match_df
 
     def scrape_match_data(self, years, from_date=None, to_date=pd.to_datetime('today').normalize(), save_to_filepath=None):   
         options = Options()
