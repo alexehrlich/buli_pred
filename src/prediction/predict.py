ecea7b70485bb80265a5987dda7d881002a9ea44
Alexander Ehrlich <50752674+alexehrlich@users.noreply.github.com>
2025-08-19 14:11:28 +0200

    load features from files

--- src/prediction/predict.py
+++ src/prediction/predict.py
@@ -1,71 +1,18 @@
 import pandas as pd
 import pickle
+import json
 
 def main():
     df = pd.read_csv('./data/processed/buli_matches_rolling.csv')
+
+    #only load all not played matches to make prediction
     open_matches_df = df[df['result_home'].isna()]
 
-    features_complex = [
-    "xg_home_rolling_3",
-    "xg_away_rolling_3",
-    "elo_home_rolling_3",
-    "elo_away_rolling_3",
-    "elo_rolling_diff",           # Combined team strength
-    "xg_rolling_diff",            # Attack quality difference
-    "goal_ratio_diff",            # Recent scoring form
-    "ga_per_xga_diff",            # Defense efficiency diff
-    "gf_per_xg_diff",             # Offense efficiency diff
-    "elo_diff_x_xg_diff",         # Interaction term, might capture outliers --> draws?
-    "elo_diff_sq",
+    with open('./src/training/features_simple_model.json', 'r') as f:
+        features_simple = json.load(f)
 
-    "days_since_home",            # Rest days (can impact performance)
-    "days_since_away",
-
-    "goal_ratio_home_rolling_1",
-    "goal_ratio_away_rolling_1",
-
-    "match_hour",
-    "day_code",
-    "week",
-
-    "poss_home_rolling_3",
-    "sot_home_rolling_3",
-    "poss_away_rolling_3",
-    "sot_away_rolling_3",
-    "xg_away_rolling_3",
-    "xg_home_rolling_3"
-    ]
-
-    features_simple = [
-    "xg_home_rolling_3",
-    "xg_away_rolling_3",
-    "elo_home_rolling_3",
-    "elo_away_rolling_3",
-    "elo_rolling_diff",           # Combined team strength
-    "xg_rolling_diff",            # Attack quality difference
-    "goal_ratio_diff",            # Recent scoring form
-    "ga_per_xga_diff",            # Defense efficiency diff
-    "gf_per_xg_diff",             # Offense efficiency diff
-    "elo_diff_x_xg_diff",         # Interaction term, might capture outliers --> draws?
-    "elo_diff_sq",
-
-    # "days_since_home",            # Rest days (can impact performance)
-    # "days_since_away",
-
-    "goal_ratio_home_rolling_1",
-    "goal_ratio_away_rolling_1",
-
-    # "match_hour",
-    # "day_code",
-    # "week",
-
-    # "poss_home_rolling_3",
-    # "sot_home_rolling_3",
-    # "poss_away_rolling_3",
-    # "sot_away_rolling_3",
-    # "xg_away_rolling_3",
-    # "xg_home_rolling_3"
-    ]
+    with open('./src/training/features_complex_model.json', 'r') as f:
+        features_complex = json.load(f)
 
     with open('./models/model_win_loose.pkl', 'rb') as f:
         simple_model = pickle.load(f)
@@ -73,7 +20,7 @@ def main():
     with open('./models/model_win_draw_loose.pkl', 'rb') as f:
         complex_model = pickle.load(f)
 
-    for idx, match in open_matches_df.iterrows():
+    for _, match in open_matches_df.iterrows():
         print(f"{match['team_home']} vs. {match['team_away']} on {match['date']}")
         X_simple = match[features_simple].to_numpy().reshape(1, -1)
         X_complex = match[features_complex].to_numpy().reshape(1, -1)
@@ -81,9 +28,7 @@ def main():
         resukt_complex = complex_model.predict_proba(X_complex)
         print(f"{match['team_home']} wins: {result_simple[0][1] * 100:.2f}%")
         print(f"{match['team_home']} looses OR draw: {result_simple[0][0] * 100:.2f}%")
-        print(f"Actual home Team result: {match['result_home']}")
         print("\n")
-
 
 if __name__ == '__main__':
     main()
--- src/training/train.py
+++ src/training/train.py
@@ -8,6 +8,7 @@ from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 import torch
 from sklearn import metrics
 from xgboost import XGBClassifier
+import json
 
 def evaluate_model(model, X_train, X_test, y_train, y_test, labels=['H', 'D', 'A'], y_pred_override=None):
     # Predict train and test
@@ -30,43 +31,23 @@ def evaluate_model(model, X_train, X_test, y_train, y_test, labels=['H', 'D', 'A
     disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=labels)
     disp.plot()
 
+def time_series_split(df, features, target, train_size = 0.8) -> pd.DataFrame:
+    split_idx = int(len(df) * train_size)
+    train = df.iloc[:split_idx]
+    test = df.iloc[split_idx:]
+    X_train = train[features].to_numpy()
+    y_train = train[target].to_numpy()
+    X_test = test[features].to_numpy()
+    y_test = test[target].to_numpy()
+
+    return X_train, X_test, y_train, y_test
+
 def train_simple_model(df):
-    features = [
-    "xg_home_rolling_3",
-    "xg_away_rolling_3",
-    "elo_home_rolling_3",
-    "elo_away_rolling_3",
-    "elo_rolling_diff",           # Combined team strength
-    "xg_rolling_diff",            # Attack quality difference
-    "goal_ratio_diff",            # Recent scoring form
-    "ga_per_xga_diff",            # Defense efficiency diff
-    "gf_per_xg_diff",             # Offense efficiency diff
-    "elo_diff_x_xg_diff",         # Interaction term, might capture outliers --> draws?
-    "elo_diff_sq",
+    print('Training the simple model:')
+    with open('./src/training/features_simple_model.json', 'r') as f:
+        features = json.load(f)
 
-    # "days_since_home",            # Rest days (can impact performance)
-    # "days_since_away",
-
-    "goal_ratio_home_rolling_1",
-    "goal_ratio_away_rolling_1",
-
-    # "match_hour",
-    # "day_code",
-    # "week",
-
-    # "poss_home_rolling_3",
-    # "sot_home_rolling_3",
-    # "poss_away_rolling_3",
-    # "sot_away_rolling_3",
-    # "xg_away_rolling_3",
-    # "xg_home_rolling_3"
-    ]
-
-    cutoff_date = pd.to_datetime('2024-05-01')
-    X_train = df[df['date'] < cutoff_date][features].to_numpy()
-    y_train = df[df['date'] < cutoff_date]['home_won'].to_numpy()
-    X_test = df[df['date'] >= cutoff_date][features].to_numpy()
-    y_test = df[df['date'] >= cutoff_date]['home_won'].to_numpy()
+    X_train, X_test, y_train, y_test = time_series_split(df, features, 'home_won')
 
 
     print(F"X_train_shape: {X_train.shape},  y_train_shape: {y_train.shape}")
@@ -93,42 +74,11 @@ def train_simple_model(df):
         pickle.dump(xgb_clf, f)
 
 def train_complex_model(df):
-    features = [
-    "xg_home_rolling_3",
-    "xg_away_rolling_3",
-    "elo_home_rolling_3",
-    "elo_away_rolling_3",
-    "elo_rolling_diff",           # Combined team strength
-    "xg_rolling_diff",            # Attack quality difference
-    "goal_ratio_diff",            # Recent scoring form
-    "ga_per_xga_diff",            # Defense efficiency diff
-    "gf_per_xg_diff",             # Offense efficiency diff
-    "elo_diff_x_xg_diff",         # Interaction term, might capture outliers --> draws?
-    "elo_diff_sq",
+    print('Training the complex model:')
+    with open('./src/training/features_complex_model.json', 'r') as f:
+        features = json.load(f)
 
-    "days_since_home",            # Rest days (can impact performance)
-    "days_since_away",
-
-    "goal_ratio_home_rolling_1",
-    "goal_ratio_away_rolling_1",
-
-    "match_hour",
-    "day_code",
-    "week",
-
-    "poss_home_rolling_3",
-    "sot_home_rolling_3",
-    "poss_away_rolling_3",
-    "sot_away_rolling_3",
-    "xg_away_rolling_3",
-    "xg_home_rolling_3"
-    ]
-
-    cutoff_date = pd.to_datetime('2024-05-01')
-    X_train = df[df['date'] < cutoff_date][features].to_numpy()
-    y_train = df[df['date'] < cutoff_date]['target'].to_numpy()
-    X_test = df[df['date'] >= cutoff_date][features].to_numpy()
-    y_test = df[df['date'] >= cutoff_date]['target'].to_numpy()
+    X_train, X_test, y_train, y_test = time_series_split(df, features, 'target')
 
     print(F"X_train_shape: {X_train.shape},  y_train_shape: {y_train.shape}")
     print(F"X_test_shape: {X_test.shape},  y_test_shape: {y_test.shape}")
@@ -156,6 +106,8 @@ def train_complex_model(df):
 
 def main():
     df = pd.read_csv('./data/processed/buli_matches_rolling.csv', index_col=0)
+
+    #only use already played matches for training
     df = df[df['result_home'].notna()]
     df['target'] = df['result_home'].map({"W": 0, "D":1, "L":2})
     df['date'] = pd.to_datetime(df['date'])
